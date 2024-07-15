51ca3362392683f852dc41141cd482b8e65dca82
HoneyPradhan <130152598+HoneyPradhan@users.noreply.github.com>
2024-07-15 15:54:54 +0530

    Update Login.js

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  // form function
+  // form functions
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
